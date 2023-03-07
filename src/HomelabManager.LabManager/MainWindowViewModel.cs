76c7a6255bedae4a5fdaa930587236c7f21722bc
Owen Shelton <jowenshelton@gmail.com>
2023-03-06 21:57:02 -0500

    Add folder button for repo data path to main window.

--- src/HomelabManager.LabManager/MainWindowViewModel.cs
+++ src/HomelabManager.LabManager/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using HomeLabManager.Common.Data.CoreConfiguration;
+﻿using System.Diagnostics;
+using HomeLabManager.Common.Data.CoreConfiguration;
 using HomeLabManager.Manager.Services.Navigation;
 using HomeLabManager.Manager.Services.Navigation.Requests;
 using Microsoft.Extensions.DependencyInjection;
@@ -13,16 +14,43 @@ public sealed class MainWindowViewModel: ReactiveObject
 {
     public MainWindowViewModel()
     {
-        Program.ServiceProvider!.Services.GetService<ICoreConfigurationManager>()!.GetOrCreateCoreConfiguration(() => new CoreConfigurationDto
+        var coreConfigurationManager = Program.ServiceProvider!.Services.GetService<ICoreConfigurationManager>()!;
+        coreConfigurationManager.GetOrCreateCoreConfiguration(() => new CoreConfigurationDto
         {
             GitConfigFilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".gitconfig"),
             GithubPat = "",
             GithubUserName = "",
             HomeLabRepoDataPath = "",
         });
+        coreConfigurationManager.CoreConfigurationUpdated.Subscribe(UpdateConfigProperties);
+
+        UpdateConfigProperties(coreConfigurationManager.GetCoreConfiguration());
 
         _navigationService = Program.ServiceProvider!.Services.GetService<INavigationService>()!;
     }
+
+    /// <summary>
+    /// Whether or not the repo data path point to a valid directory.
+    /// </summary>
+    public bool HasRepoDataPath
+    {
+        get => _hasRepoDataPath;
+        set => this.RaiseAndSetIfChanged(ref _hasRepoDataPath, value);
+    }
+
+    /// <summary>
+    /// Path to the data repo.
+    /// </summary>
+    public string? RepoDataPath
+    {
+        get => _repoDataPath;
+        set => this.RaiseAndSetIfChanged(ref _repoDataPath, value);
+    }
+
+    /// <summary>
+    /// Reference to the Navigation Service.
+    /// </summary>
+    public INavigationService NavigationService => _navigationService;
 
     /// <summary>
     /// Once the window loads navigate to the Home Page.
@@ -32,10 +60,25 @@ public sealed class MainWindowViewModel: ReactiveObject
         await NavigationService.NavigateTo(new HomeNavigationRequest()).ConfigureAwait(false);
     }
 
-    /// <summary>
-    /// Reference to the Navigation Service.
-    /// </summary>
-    public INavigationService NavigationService => _navigationService;
+    public void OpenRepoDataPath()
+    {
+        ProcessStartInfo startInfo = new ProcessStartInfo
+        {
+            Arguments = RepoDataPath,
+            FileName = "explorer.exe"
+        };
+
+        Process.Start(startInfo);
+    }
+
+    private void UpdateConfigProperties(CoreConfigurationDto config)
+    {
+        HasRepoDataPath = Directory.Exists(config.HomeLabRepoDataPath);
+        RepoDataPath = config.HomeLabRepoDataPath;
+    }
 
     private readonly INavigationService _navigationService;
+
+    private bool _hasRepoDataPath;
+    private string? _repoDataPath;
 }
