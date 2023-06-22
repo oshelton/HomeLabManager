6d931c453ef5dcd86ad4b31cee718c918b139f9d
oshelton <jowenshelton@gmail.com>
2023-06-22 08:04:41 -0400

    Add basic string form field support and styling.

---
+++ src/HomelabManager.LabManager/Controls/BasicStringFormField.cs
@@ -0,0 +1,24 @@
+﻿using Avalonia;
+
+namespace HomeLabManager.Manager.Controls;
+
+/// <summary>
+/// Simple string based form field class.
+/// </summary>
+public class BasicStringFormField : FormField
+{
+    /// <summary>
+    /// Defines the <see cref="Value"/> property.
+    /// </summary>
+    public static readonly StyledProperty<string> StringValueProperty =
+        AvaloniaProperty.Register<FormField, string>(nameof(StringValue), null);
+
+    /// <summary>
+    /// Gets or sets a value for the Field's Label.
+    /// </summary>
+    public string StringValue
+    {
+        get => GetValue(StringValueProperty);
+        set => SetValue(StringValueProperty, value);
+    }
+}
