8ec4c02d7b3b69511de6920359c7995ffbf9782c
oshelton <jowenshelton@gmail.com>
2023-06-30 08:18:30 -0400

    Create initial create/edit server page and view model plumbing.

--- src/HomelabManager.LabManager/Pages/CreateEditServer/CreateEditServerViewModel.cs
+++ src/HomelabManager.LabManager/Pages/CreateEditServer/CreateEditServerViewModel.cs
@@ -12,9 +12,17 @@ namespace HomeLabManager.Manager.Pages.CreateEditServer;
 /// <summary>
 /// Server Listing Page View Model.
 /// </summary>
-public sealed class CreateEditServerViewModel : PageBaseViewModel
+public sealed class CreateEditServerViewModel : ValidatedPageBaseViewModel
 {
+    // Design time constructor.
+    public CreateEditServerViewModel()
+    {
+        _serverDataManager = Program.ServiceProvider.Services.GetService<IServerDataManager>();
+        _navigationService = Program.ServiceProvider.Services.GetService<INavigationService>();
+    }
+
     public CreateEditServerViewModel(bool isNew, BaseServerDto server)
+        : this()
     {
         if (server is null)
             throw new ArgumentNullException(nameof(server));
@@ -35,6 +43,43 @@ public sealed class CreateEditServerViewModel : PageBaseViewModel
     }
 
     public override Task<bool> TryNavigateAway() => Task.FromResult(true);
+
+    public async Task SaveChangesAndNavigateBack()
+    {
+        IsSaving = true;
+
+        var (dialog, dialogTask) = SharedDialogs.ShowSimpleSavingDataDialog("Saving Core Configuration Changes...");
+
+        // Do the actual saving work.
+        //await Task.Run(() => ).ConfigureAwait(true);
+
+        dialog.GetWindow().Close();
+
+        IsSaving = false;
+        HasChanges = false;
+
+        await _navigationService!.NavigateBack().ConfigureAwait(false);
+    }
+
+    public INavigationService NavigationService => _navigationService;
+
+    public bool HasChanges
+    {
+        get => _hasChanges;
+        private set => this.RaiseAndSetIfChanged(ref _hasChanges, value);
+    }
+
+    public bool IsSaving
+    {
+        get => _isSaving;
+        private set => this.RaiseAndSetIfChanged(ref _isSaving, value);
+    }
+
+    private readonly IServerDataManager _serverDataManager;
+    private readonly INavigationService _navigationService;
+
+    private bool _hasChanges;
+    private bool _isSaving;
 
     private readonly bool _isNew;
     private readonly bool _isEditingServerHost;
