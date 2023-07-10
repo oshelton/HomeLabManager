49a207aa075ed5caf9804030b1b8652d6f2e965f
oshelton <jowenshelton@gmail.com>
2023-07-09 21:40:36 -0400

    Add proper validation support to BasicStringFormFields.

--- src/HomelabManager.LabManager/Controls/BasicStringFormField.cs
+++ src/HomelabManager.LabManager/Controls/BasicStringFormField.cs
@@ -1,4 +1,6 @@
 ﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Data;
 
 namespace HomeLabManager.Manager.Controls;
 
@@ -10,15 +12,32 @@ public class BasicStringFormField : FormField
     /// <summary>
     /// Defines the <see cref="Value"/> property.
     /// </summary>
-    public static readonly StyledProperty<string> StringValueProperty =
-        AvaloniaProperty.Register<FormField, string>(nameof(StringValue), null);
+    public static readonly DirectProperty<BasicStringFormField, string> StringValueProperty =
+        AvaloniaProperty.RegisterDirect<BasicStringFormField, string>(
+            nameof(StringValue),
+            o => o.StringValue,
+            (o, v) => o.StringValue = v,
+            defaultBindingMode: Avalonia.Data.BindingMode.TwoWay,
+            enableDataValidation: true);
 
+    private string _stringValue;
     /// <summary>
-    /// Gets or sets a value for the Field's Label.
+    /// Gets or sets a value for the Field's value.
     /// </summary>
     public string StringValue
     {
-        get => GetValue(StringValueProperty);
-        set => SetValue(StringValueProperty, value);
+        get => _stringValue;
+        set => SetAndRaise(StringValueProperty, ref _stringValue, value);
+    }
+
+    protected override void UpdateDataValidation(
+            AvaloniaProperty property,
+            BindingValueType state,
+            Exception error)
+    {
+        if (property == StringValueProperty)
+        {
+            DataValidationErrors.SetError(this, error);
+        }
     }
 }
