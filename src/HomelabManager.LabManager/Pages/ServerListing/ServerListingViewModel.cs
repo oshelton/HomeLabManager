7298073cea408c39ed7f726a2d6719cb12d4d4b1
oshelton <jowenshelton@gmail.com>
2023-08-02 21:41:58 -0400

    Add support for moving, esiting, and deleting servers.

--- src/HomelabManager.LabManager/Pages/ServerListing/ServerListingViewModel.cs
+++ src/HomelabManager.LabManager/Pages/ServerListing/ServerListingViewModel.cs
@@ -1,10 +1,16 @@
-﻿using System.Reactive;
+﻿using System.Collections.ObjectModel;
+using System.Reactive;
 using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
 using Avalonia.Collections;
+using DynamicData;
+using DynamicData.Binding;
 using HomeLabManager.Common.Data.CoreConfiguration;
 using HomeLabManager.Common.Data.Git.Server;
 using HomeLabManager.Manager.Services.Navigation;
 using HomeLabManager.Manager.Services.Navigation.Requests;
+using HomeLabManager.Manager.Utils;
 using Microsoft.Extensions.DependencyInjection;
 using ReactiveUI;
 
@@ -40,7 +46,8 @@ public class ServerListingViewModel : PageBaseViewModel
                     CurrentDisplayMode = ServerListingDisplayMode.NoServers;
                     break;
                 case 2:
-                    _servers = new AvaloniaList<ServerViewModel>(_serverDataManager.GetServers().Select(x => new ServerViewModel(x)).ToArray());
+                    var servers = _serverDataManager.GetServers();
+                    _serverCache.AddOrUpdate(servers.Select(x => new ServerViewModel(x, this, servers.Count)));
                     CurrentDisplayMode = ServerListingDisplayMode.HasServers;
                     break;
             }
@@ -50,6 +57,28 @@ public class ServerListingViewModel : PageBaseViewModel
 
         CreateNewServerHostCommand = ReactiveCommand.CreateFromTask<int?>(CreateNewServerHost)
             .DisposeWith(_disposables);
+
+        EditServerCommand = ReactiveCommand.CreateFromTask<ServerViewModel>(EditServerHost)
+            .DisposeWith(_disposables);
+
+        MoveServerUpCommand = ReactiveCommand.CreateFromTask<ServerViewModel>(MoveServerHostUp)
+            .DisposeWith(_disposables);
+
+        MoveServerDownCommand = ReactiveCommand.CreateFromTask<ServerViewModel>(MoveServerHostDown)
+            .DisposeWith(_disposables);
+
+        DeleteServerCommand = ReactiveCommand.CreateFromTask<ServerViewModel>(DeleteServerHost)
+            .DisposeWith(_disposables);
+
+        ServerListObservable = _serverCache.Connect().Publish();
+
+        ServerListObservable.AutoRefresh(server => server.DisplayIndex, propertyChangeThrottle: TimeSpan.FromMilliseconds(10))
+            .SortBy(x => x.DisplayIndex)
+            .Bind(out _sortedServers)
+            .Subscribe()
+            .DisposeWith(_disposables);
+
+        _disposables.Add(ServerListObservable.Connect());
     }
 
     public override string Title => "Server Listing";
@@ -62,21 +91,47 @@ public class ServerListingViewModel : PageBaseViewModel
         // Load Servers.
         CurrentDisplayMode = ServerListingDisplayMode.IsLoading;
 
-        AvaloniaList<ServerViewModel> servers = null;
         await Task.Run(async () =>
         {
-            servers = new AvaloniaList<ServerViewModel>(_serverDataManager.GetServers()
-                .Select(x => new ServerViewModel(x))
-                .OrderBy(x => x.DisplayIndex));
+            var servers = _serverDataManager.GetServers();
+            var serverViewModels = servers.Select(x => new ServerViewModel(x, this, servers.Count)).OrderBy(x => x.DisplayIndex);
+            foreach (var server in serverViewModels)
+            {
+                _disposables.Add(server);
+            }
+            _serverCache.Clear();
+            _serverCache.AddOrUpdate(serverViewModels);
         }).ConfigureAwait(true);
 
-        Servers = servers;
-        CurrentDisplayMode = (servers?.Count ?? 0) != 0 ? ServerListingDisplayMode.HasServers : ServerListingDisplayMode.NoServers;
+        CurrentDisplayMode = _serverCache.Count != 0 ? ServerListingDisplayMode.HasServers : ServerListingDisplayMode.NoServers;
     }
 
     public override Task<bool> TryNavigateAway() => Task.FromResult(true);
 
+    /// <summary>
+    /// Command to create a ner server at the specified location.
+    /// </summary>
     public ReactiveCommand<int?, Unit> CreateNewServerHostCommand { get; }
+
+    /// <summary>
+    /// Command to edit a server.
+    /// </summary>
+    public ReactiveCommand<ServerViewModel, Unit> EditServerCommand { get; }
+
+    /// <summary>
+    /// Command to move a server up in the ordering.
+    /// </summary>
+    public ReactiveCommand<ServerViewModel, Unit> MoveServerUpCommand { get; }
+
+    /// <summary>
+    /// Move server down in the ordering.
+    /// </summary>
+    public ReactiveCommand<ServerViewModel, Unit> MoveServerDownCommand { get; }
+
+    /// <summary>
+    /// Delete server command.
+    /// </summary>
+    public ReactiveCommand<ServerViewModel, Unit> DeleteServerCommand { get; }
 
     /// <summary>
     /// Current display mode.
@@ -88,13 +143,14 @@ public class ServerListingViewModel : PageBaseViewModel
     }
 
     /// <summary>
-    /// Collection of servers.
+    /// An observable to hook into changes to the list of servers.
     /// </summary>
-    public virtual IAvaloniaReadOnlyList<ServerViewModel> Servers
-    {
-        get => _servers;
-        private set => this.RaiseAndSetIfChanged(ref _servers, (AvaloniaList<ServerViewModel>)value);
-    }
+    public IConnectableObservable<IChangeSet<ServerViewModel, Guid>> ServerListObservable { get; }
+
+    /// <summary>
+    /// Collection of sorted servers.
+    /// </summary>
+    public virtual ReadOnlyObservableCollection<ServerViewModel> SortedServers => _sortedServers;
 
     protected override void Dispose(bool isDisposing) => _disposables.Dispose();
 
@@ -109,6 +165,90 @@ public class ServerListingViewModel : PageBaseViewModel
         VMs = Array.Empty<ServerVmDto>(),
     }, afterIndex));
 
+    /// <summary>
+    /// Edit an existing server.
+    /// </summary>
+    private Task EditServerHost(ServerViewModel server)
+    {
+        if (server is null)
+            throw new ArgumentNullException(nameof(server));
+
+        return _navigationService.NavigateTo(new CreateEditServerNavigationRequest(false, server.ToDto()));
+    }
+
+    /// <summary>
+    /// Move passed in server up in the ordering.
+    /// </summary>
+    private Task MoveServerHostUp(ServerViewModel server)
+    {
+        if (server is null)
+            throw new ArgumentNullException(nameof(server));
+        if (server.DisplayIndex == 0)
+            throw new ArgumentException($"{nameof(server)} cannot be moved up if it is first!");
+
+        var previous = SortedServers.First(x => x.DisplayIndex == server.DisplayIndex - 1);
+        previous.DisplayIndex++;
+        server.DisplayIndex--;
+
+        return Task.Run(() =>
+        {
+            _serverDataManager.AddUpdateServer(previous.ToDto());
+            _serverDataManager.AddUpdateServer(server.ToDto());
+        });
+    }
+
+    /// <summary>
+    /// Move passed in server down in the ordering.
+    /// </summary>
+    private Task MoveServerHostDown(ServerViewModel server)
+    {
+        if (server is null)
+            throw new ArgumentNullException(nameof(server));
+        if (server.DisplayIndex == SortedServers.Count - 1)
+            throw new ArgumentException($"{nameof(server)} cannot be moved down if it is last!");
+
+        var next = SortedServers.First(x => x.DisplayIndex == server.DisplayIndex + 1);
+        next.DisplayIndex--;
+        server.DisplayIndex++;
+
+        return Task.Run(() =>
+        {
+            _serverDataManager.AddUpdateServer(server.ToDto());
+            _serverDataManager.AddUpdateServer(next.ToDto());
+        });
+    }
+
+    /// <summary>
+    /// Delete an existing server.
+    /// </summary>
+    private async Task DeleteServerHost(ServerViewModel server)
+    {
+        if (server is null)
+            throw new ArgumentNullException(nameof(server));
+
+        var continueDeleting = await Utils.SharedDialogs.ShowSimpleYesNoDialog("Deleting cannot be readily undone outside of Git.\nAre you sure?").ConfigureAwait(true);
+
+        if (!continueDeleting)
+            return;
+
+        var (dialog, dialogTask) = SharedDialogs.ShowSimpleSavingDataDialog("Deleting Server...");
+
+        var toUpdateServers = SortedServers.Where(x => x.DisplayIndex > server.DisplayIndex).ToList();
+        await Task.Run(() =>
+        {
+            _serverDataManager.DeleteServer(server.ToDto());
+            foreach (var toUpdate in toUpdateServers)
+            {
+                toUpdate.DisplayIndex--;
+                _serverDataManager.AddUpdateServer(toUpdate.ToDto());
+            }
+        }).ConfigureAwait(true);
+
+        _serverCache.Remove(server);
+
+        dialog.GetWindow().Close();
+    }
+
     private readonly ICoreConfigurationManager _coreConfigurationManager;
     private readonly IServerDataManager _serverDataManager;
     private readonly INavigationService _navigationService;
@@ -117,5 +257,6 @@ public class ServerListingViewModel : PageBaseViewModel
 
     // Property backing fields.
     private ServerListingDisplayMode _currentDisplayMode;
-    private AvaloniaList<ServerViewModel> _servers;
+    private SourceCache<ServerViewModel, Guid> _serverCache = new SourceCache<ServerViewModel, Guid>(x => x.UniqueId);
+    private ReadOnlyObservableCollection<ServerViewModel> _sortedServers;
 }
--- src/HomelabManager.LabManager/Pages/ServerListing/ServerViewModel.cs
+++ src/HomelabManager.LabManager/Pages/ServerListing/ServerViewModel.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
+using System.Reactive;
 using System.Reactive.Disposables;
+using System.Reactive.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Avalonia.Collections;
@@ -10,19 +13,109 @@ using ReactiveUI;
 
 namespace HomeLabManager.Manager.Pages.ServerListing;
 
-public sealed class ServerViewModel : ReactiveObject
+public sealed class ServerViewModel : ReactiveObject, IDisposable
 {
-    public ServerViewModel(ServerHostDto dto)
+    /// <summary>
+    /// Main constructor.
+    /// </summary>
+    /// <param name="dto">Dto backing the view model.</param>
+    public ServerViewModel(ServerHostDto dto, ServerListingViewModel listingViewModel, int startingServerCount)
     {
-        if (dto is null)
-            throw new ArgumentNullException(nameof(dto));
+        _dto = dto ?? throw new ArgumentNullException(nameof(dto));
+        ParentListingViewModel = listingViewModel ?? throw new ArgumentNullException(nameof(listingViewModel));
 
-        DisplayName = dto.Metadata.DisplayName;
-        Name = dto.Metadata.Name;
-        DisplayIndex = dto.Metadata.DisplayIndex;
+        _displayIndex = _dto.Metadata.DisplayIndex;
+
+        _disposables = new CompositeDisposable();
+
+        // Set up an observable to tracke whether or not this server can be moved up.
+        _canMoveUp = this.WhenAnyValue(x => x.DisplayIndex, displayIndex => displayIndex > 0)
+            .ToProperty(this, nameof(CanMoveUp))
+            .DisposeWith(_disposables);
+
+        // Set up an observable to tracke whether or not this server can be moved down.
+        _canMoveDown = Observable.CombineLatest(this.WhenAnyValue(x => x.DisplayIndex), 
+                        Observable.FromEventPattern<NotifyCollectionChangedEventHandler, NotifyCollectionChangedEventArgs>(
+                            h => ((INotifyCollectionChanged) ParentListingViewModel.SortedServers).CollectionChanged += h,
+                            h => ((INotifyCollectionChanged) ParentListingViewModel.SortedServers).CollectionChanged -= h)
+                            .Select(handlerArgs => ParentListingViewModel.SortedServers.Count), 
+                        (index, serverCount) => index < serverCount - 1)
+            .StartWith(_displayIndex < startingServerCount - 1)
+            .ToProperty(this, nameof(CanMoveDown))
+            .DisposeWith(_disposables);
+
     }
 
-    public string DisplayName { get; }
-    public string Name { get; }
-    public int DisplayIndex { get; }
+    /// <summary>
+    /// Design time default constructor.
+    /// </summary>
+    public ServerViewModel()
+        : this(new ServerHostDto
+        {
+            Metadata = new ServerMetadataDto
+            {
+                DisplayName = "Test Display Name",
+                Name = "TEST-HOST-NAME",
+                DisplayIndex = 5,
+            }
+        }, new ServerListingViewModel(), 0) { }
+
+    /// <summary>
+    /// Cleanup the view model.
+    /// </summary>
+    public void Dispose() => _disposables.Dispose();
+
+    /// <summary>
+    /// Unique Id of the Server.
+    /// </summary>
+    public Guid UniqueId => _dto.UniqueId ?? Guid.Empty;
+
+    /// <summary>
+    /// Display name of the Server.
+    /// </summary>
+    public string DisplayName => _dto.Metadata.DisplayName;
+
+    /// <summary>
+    /// Name/Host Name of the server.
+    /// </summary>
+    public string Name => _dto.Metadata.Name;
+
+    /// <summary>
+    /// Display index/order of the server.
+    /// </summary>
+    public int DisplayIndex
+    {
+        get => _displayIndex;
+        set => this.RaiseAndSetIfChanged(ref _displayIndex, value);
+    }
+
+    /// <summary>
+    /// Whether or not this server can be moved up.
+    /// </summary>
+    public bool CanMoveUp => _canMoveUp.Value;
+
+    /// <summary>
+    /// Whether or not this server can be moved down.
+    /// </summary>
+    public bool CanMoveDown => _canMoveDown.Value;
+
+    /// <summary>
+    /// Reference back to the parent listing view model.
+    /// </summary>
+    public ServerListingViewModel ParentListingViewModel { get; }
+
+    /// <summary>
+    /// Reconstruct a dto from this Server view model.
+    /// </summary>
+    public ServerHostDto ToDto() => _dto with
+    {
+        Metadata = _dto.Metadata with { DisplayIndex = DisplayIndex },
+    };
+
+    private readonly ServerHostDto _dto;
+    private readonly CompositeDisposable _disposables;
+    private readonly ObservableAsPropertyHelper<bool> _canMoveUp;
+    private readonly ObservableAsPropertyHelper<bool> _canMoveDown;
+
+    private int _displayIndex;
 }
