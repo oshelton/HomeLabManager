254d676031cca95e996fcacf6b2d928011184d9f
oshelton <jowenshelton@gmail.com>
2023-08-06 09:05:14 -0400

    #19 - Get Home Page View Model Tests working with mocked services.

--- tests/HomeLabManager.ManagerTests/Tests/Pages/HomeViewModelTests.cs
+++ tests/HomeLabManager.ManagerTests/Tests/Pages/HomeViewModelTests.cs
@@ -1,5 +1,9 @@
-﻿using HomeLabManager.Manager.Pages.Home;
+﻿using HomeLabManager.Common.Data.CoreConfiguration;
+using HomeLabManager.Common.Data.Git.Server;
+using HomeLabManager.Manager.Pages.Home;
+using HomeLabManager.Manager.Services.Navigation;
 using HomeLabManager.Manager.Services.Navigation.Requests;
+using Moq;
 
 namespace HomeLabManager.ManagerTests.Tests.Pages;
 
@@ -9,13 +13,13 @@ namespace HomeLabManager.ManagerTests.Tests.Pages;
 public class HomeViewModelTests
 {
     [SetUp]
-    public void SetUp() => Utils.RegisterTestServices();
+    public void SetUp() => _services = Utils.RegisterTestServices();
 
     /// <summary>
     /// Test view model creation and initial state.
     /// </summary>
     [Test]
-    public void TestCreation()
+    public void Constructor_DefaultConstruction_TestDefaultHomePageConstructionExpectations()
     {
         var homePage = new HomeViewModel();
 
@@ -30,10 +34,44 @@ public class HomeViewModelTests
     /// Test the logic executed when the page is navigated to.
     /// </summary>
     [Test]
-    public async Task TestNavigatingTo()
+    public async Task NavigateTo_HappyPathNavigateTo_ConfirmNavigatingToExpectationsWithFullConfiguration()
     {
-        var homePage = new HomeViewModel();
+        _services.MockCoreConfigManager.Setup(x => x.GetCoreConfiguration())
+            .Returns(new CoreConfigurationDto
+            {
+                HomeLabRepoDataPath = Path.GetTempPath(),
+            });
 
+        _services.MockServerDatamanager.Setup(x => x.GetServers())
+            .Returns(new[]
+            {
+                new ServerHostDto
+                {
+                    Metadata = new ServerMetadataDto
+                    {
+                        DisplayName = "Test 1",
+                        Name = "HOST-1"
+                    }
+                },
+                new ServerHostDto
+                {
+                    Metadata = new ServerMetadataDto
+                    {
+                        DisplayName = "Test 2",
+                        Name = "HOST-2"
+                    }
+                },
+                new ServerHostDto
+                {
+                    Metadata = new ServerMetadataDto
+                    {
+                        DisplayName = "Test 3",
+                        Name = "HOST-3"
+                    }
+                }
+            });
+
+        var homePage = new HomeViewModel();
         var navigateTask = homePage.NavigateTo(new HomeNavigationRequest());
 
         Assert.That(homePage.CurrentDisplayMode, Is.EqualTo(HomeDisplayMode.IsLoading));
@@ -45,4 +83,10 @@ public class HomeViewModelTests
 
         homePage.Dispose();
     }
+
+    private (
+        Mock<ICoreConfigurationManager> MockCoreConfigManager, 
+        Mock<IServerDataManager> MockServerDatamanager, 
+        Mock<INavigationService> MockNavigationService
+    ) _services;
 }
