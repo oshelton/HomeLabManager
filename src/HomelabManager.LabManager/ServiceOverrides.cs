adc34a5b16d6615680076c7b5f39e0ed7491567f
oshelton <jowenshelton@gmail.com>
2023-08-06 08:47:17 -0400

    #19 - Remove no longer desirable test services and add mocking support to them.

---
+++ src/HomelabManager.LabManager/ServiceOverrides.cs
@@ -0,0 +1,16 @@
+﻿using HomeLabManager.Common.Data.CoreConfiguration;
+using HomeLabManager.Common.Data.Git.Server;
+using HomeLabManager.Manager.Services.Navigation;
+
+namespace HomeLabManager.Manager
+{
+    /// <summary>
+    /// Class for providing service overrides for testing purposes.
+    /// </summary>
+    public sealed class ServiceOverrides
+    {
+        public Func<ICoreConfigurationManager> CoreConfigurationManagerServiceBuilder { get; init; }
+        public Func<IServerDataManager> ServerDataManagerServiceBuilder { get; init; }
+        public Func<INavigationService> NavigationServiceBuilder { get; init; }
+    }
+}
---
+++ tests/HomeLabManager.ManagerTests/mockserviceextensions/MockCoreConfigurationManagerExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Subjects;
+using System.Text;
+using System.Threading.Tasks;
+using HomeLabManager.Common.Data.CoreConfiguration;
+using Moq;
+
+namespace HomeLabManager.ManagerTests.MockServiceExtensions;
+
+/// <summary>
+/// Class for commonly used mocking helpers for the Core Configuration manager service.
+/// </summary>
+internal static class MockCoreConfigurationManagerExtensions
+{
+    /// <summary>
+    /// Initiallize an ICoreConfigurationManager mock with the bare required functionality to get a simple test to run.
+    /// </summary>
+    public static void InitializeMinimumRequiredConfiguration(this Mock<ICoreConfigurationManager> mockService)
+    {
+        if (mockService is null)
+            throw new ArgumentNullException(nameof(mockService));
+
+        mockService.Reset();
+
+        mockService.SetupGet(x => x.CoreConfigurationUpdated).Returns(() => new Subject<CoreConfigurationDto>());
+        mockService.Setup(x => x.GetCoreConfiguration()).Returns(() => new CoreConfigurationDto());
+    }
+}
