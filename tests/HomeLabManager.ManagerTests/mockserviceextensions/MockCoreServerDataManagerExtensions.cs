ec137f5b86bc4e8aa2e13107b32702e79656cc1b
oshelton <jowenshelton@gmail.com>
2023-08-07 11:32:30 -0400

    #19 add extension method to help create simple servers.

--- tests/HomeLabManager.ManagerTests/mockserviceextensions/MockCoreServerDataManagerExtensions.cs
+++ tests/HomeLabManager.ManagerTests/mockserviceextensions/MockCoreServerDataManagerExtensions.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using HomeLabManager.Common.Data.CoreConfiguration;
-using HomeLabManager.Common.Data.Git.Server;
+﻿using HomeLabManager.Common.Data.Git.Server;
 using Moq;
 
 namespace HomeLabManager.ManagerTests.MockServiceExtensions;
@@ -23,5 +17,35 @@ internal static class MockCoreServerDataManagerExtensions
             throw new ArgumentNullException(nameof(mockService));
 
         mockService.Reset();
+    }
+
+    /// <summary>
+    /// Setup GetServers to return a configurable number of server hosts.
+    /// </summary>
+    /// <param name="mockService">Mock service to set up.</param>
+    /// <param name="numberServers">Number of servers to create.</param>
+    /// <returns>Read only list of created servers.</returns>
+    public static IReadOnlyList<ServerHostDto> SetupSimpleServers(this Mock<IServerDataManager> mockService, int numberServers)
+    {
+        var servers = new List<ServerHostDto>();
+        for (var i = 0; i < numberServers; i++)
+        {
+            servers.Add(new ServerHostDto
+                {
+                    Metadata = new ServerMetadataDto
+                    {
+                        DisplayName = $"Test {i + 1}",
+                        Name = $"HOST-{i + 1}"
+                    },
+                    DockerCompose = new DockerComposeDto(),
+                    Configuration = new ServerConfigurationDto()
+                }
+            );
+        }
+
+        mockService.Setup(x => x.GetServers())
+            .Returns(servers);
+
+        return servers;
     }
 }
