35670b142d19b076081ba9c57493176f4f5c9608
oshelton <jowenshelton@gmail.com>
2023-08-07 11:44:23 -0400

    #19 Get Metadata editor tests working again.

--- tests/HomeLabManager.ManagerTests/Tests/Pages/CreateEditServerViewModelTests.cs
+++ tests/HomeLabManager.ManagerTests/Tests/Pages/CreateEditServerViewModelTests.cs
@@ -1,18 +1,33 @@
-﻿using HomeLabManager.Common.Data.Git.Server;
+﻿using HomeLabManager.Common.Data.CoreConfiguration;
+using HomeLabManager.Common.Data.Git.Server;
 using HomeLabManager.Manager.Pages.CreateEditServer;
-using HomeLabManager.Manager.Pages.ServerListing;
+using HomeLabManager.Manager.Services.Navigation;
 using HomeLabManager.Manager.Services.Navigation.Requests;
+using HomeLabManager.ManagerTests.MockServiceExtensions;
+using Moq;
 
 namespace HomeLabManager.ManagerTests.Tests.Pages;
 
 public sealed class MetadataViewModelTests
 {
     [SetUp]
-    public void SetUp() => Utils.RegisterTestServices();
+    public void SetUp() => _services = Utils.RegisterTestServices();
 
     [Test]
     public async Task Constructor_PageTitleVariations_ConfirmExpectedPageTitleVariations()
     {
+        var servers = _services.MockServerDataManager.SetupSimpleServers(1, _ => new[]
+            {
+                new ServerVmDto
+                {
+                    Metadata = new ServerMetadataDto
+                    {
+                        DisplayName = "VM 1",
+                        Name = "VM-1"
+                    }
+                },
+            });
+
         using (var newServer = new CreateEditServerViewModel())
         {
             var request = new CreateEditServerNavigationRequest(true, new ServerHostDto() { Metadata = new ServerMetadataDto { DisplayName = "server" } });
@@ -31,18 +46,23 @@ public sealed class MetadataViewModelTests
 
         using (var editingServer = new CreateEditServerViewModel())
         {
-            var request = new CreateEditServerNavigationRequest(false, new ServerHostDto { Metadata = new ServerMetadataDto { DisplayName = "server" } }, 5);
+            var request = new CreateEditServerNavigationRequest(false, servers[0]);
             await editingServer.NavigateTo(request).ConfigureAwait(false);
-            Assert.That(editingServer.Title, Is.EqualTo("Editing server"));
+            Assert.That(editingServer.Title, Is.EqualTo($"Editing {servers[0].Metadata.DisplayName}"));
         }
 
         using (var editingVM = new CreateEditServerViewModel())
         {
             var vm = new ServerVmDto { Metadata = new ServerMetadataDto { DisplayName = "vm" } };
-            var server = new ServerHostDto { Metadata = new ServerMetadataDto { DisplayName = "server" }, VMs = new[] { vm } };
-            var request = new CreateEditServerNavigationRequest(false, vm, 0);
+            var request = new CreateEditServerNavigationRequest(false, servers[0].VMs[0], 0);
             await editingVM.NavigateTo(request).ConfigureAwait(false);
-            Assert.That(editingVM.Title, Is.EqualTo("Editing vm"));
+            Assert.That(editingVM.Title, Is.EqualTo($"Editing {servers[0].VMs[0].Metadata.DisplayName}"));
         }
     }
+
+    private (
+        Mock<ICoreConfigurationManager> MockCoreConfigManager,
+        Mock<IServerDataManager> MockServerDataManager,
+        Mock<INavigationService> MockNavigationService
+    ) _services;
 }
